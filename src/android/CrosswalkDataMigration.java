66d2565602cf227791a8fc7e8a82f4b68f2de8a2
pynadathraju <pynadathraju@gmail.com>
2016-11-14 14:57:55 +0000

    path

--- src/android/CrosswalkDataMigration.java
+++ src/android/CrosswalkDataMigration.java
@@ -24,31 +24,3 @@ public class CrosswalkDataMigration extends CordovaPlugin {
         }
     }
 }
-/*
-package com.unit11apps.CrosswalkDataMigration;
-
-import org.apache.cordova.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-
-public class CrosswalkDataMigration extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        if (action.equals("migratedata")) {
-
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
-
-            return true;
-
-        } else {
-            
-            return false;
-
-        }
-    }
-}
-*/
